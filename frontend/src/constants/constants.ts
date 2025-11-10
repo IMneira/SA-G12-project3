bdd696d48467a72086a00ef6f518087a54171b9f
Alberto Zúñiga <azuiga@miuandes.cl>
2025-11-10 00:50:51 -0300

    Revert "Feature/dockerize frontend"

--- frontend/src/constants/constants.ts
+++
@@ -1,3 +0,0 @@
-// import env variables from a .env file or environment
-export const API_BASE_URL = process.env.API_HOST || "http://localhost:8000";
-export const SECRET_KEY = process.env.SECRET_KEY || "default_secret_key";
